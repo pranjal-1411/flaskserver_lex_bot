71b0385f02843037187096e3a962ecf392964967
pranjal-1411 <pranjalguptacse.com>
2020-05-12 19:42:48 +0530

    Succesive interaction added

--- python_files/slack/del.py
+++ python_files/slack/del.py
@@ -1,100 +1,11 @@
 
+
 import json
 import os
-import time
-from flask import jsonify
-import boto3
-import logging
-from dotenv import load_dotenv
-botName = 'AsanifyBot'
-botAlias = 'dev'
-intentName = None
+root_dir = '/mnt/f/python3resolve'
+filePath = os.path.join(root_dir,'python_files/slack/blocks/apply_leave/from_date.json')
 
-#import asyncio
-
-
-def initEnvironment( rootDir ):
-    global botName,botAlias,intentName
-    load_dotenv(os.path.join(rootDir, '.env'))
-    #botName = os.getenv('BOT_NAME')
-    #botAlias = os.getenv('BOT_ALIAS')
-    #intentName = os.getenv('INTENT_NAME')
-    os.environ['AWS_ACCESS_KEY_ID'] =  os.getenv('AWS_ACCESS_KEY_ID')
-    os.environ['AWS_SECRET_ACCESS_KEY']= os.getenv('AWS_SECRET_ACCESS_KEY')
-    os.environ['AWS_DEFAULT_REGION'] =  os.getenv('AWS_REGION')
-
-
-
-async def sendSlotValuesToLex( data=None , intentName='ApplyLeave', sender_id='12344' , useOldValue = False ):
-    data = {'type':'123455'}
-    slotValue =  data
-
-    client = boto3.client('lex-runtime')
+with open(filePath,'r') as jsFile:
+    data = json.load(jsFile)
+    print(data[0])
     
-
-    try:
-        get_session_response = client.get_session(botName= botName ,botAlias= botAlias ,userId= sender_id )
-        for recentIntent in get_session_response['recentIntentSummaryView']:
-            if recentIntent['intentName']==intentName:
-                [ slotValue.update([item]) for item in recentIntent['slots'].items() if item[1] is not None and (useOldValue or slotValue.get(item[0]) is None)  ] 
-                                
-    except Exception as e:
-        logging.error('---------------Session does not exist. Starting new session')
-        
-    client =  boto3.client('lex-runtime')
-    response = client.put_session(
-        botName=botName,
-        botAlias=botAlias,
-        userId= sender_id ,
-        sessionAttributes={},
-        dialogAction={
-            'type': 'Delegate',
-            'intentName': intentName ,
-            'slots': slotValue
-        }
-        #recentIntentSummaryView= get_session_response.get('recentIntentSummaryView') 
-    ) 
-    logging.error(response)
-    #print(response)
-    return response
-
-
-import asyncio
-
-async def count():
-    print("One")
-    await asyncio.sleep(1)
-    print("Two")
-
-
-async def main():
-    task1 = loop.create_task(sendSlotValuesToLex())
-    task2 = loop.create_task(count())
-    
-    await asyncio.wait([task1,task2])
-    
-    
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    # import time
-    # s = time.perf_counter()
-    # asyncio.run(main())
-   
-    # elapsed = time.perf_counter() - s
-    # print(f"{__file__} executed in {elapsed:0.2f} seconds.")
-    
-
-# if __name__ == "__main__":
-#     #initEnvironment( '/mnt/f/python3resolve' )
-#     data = {'type':'123455'}
-#     sender_id='123456'
-#     intentName= 'ApplyLeave' #'ApplyLeave'
-#     #asyncio.run(sendSlotValuesToLex(data,intentName,sender_id),debug=True)
-#     #response = sendSlotValuesToLex(data,intentName,sender_id)
-#     asyncio.run(temp())
-#     print("hiiiiiiiiii")
-#     #logging.error(response)
-
- 
--- python_files/slack/slack_intent_action.py
+++ python_files/slack/slack_intent_action.py
@@ -2,115 +2,23 @@
 import python_files.slack.slack_helper as slack_helper
 import requests
 import logging
+import os 
+import json
 
 def apply_leave(query):
     
     channel_id = query['event']['channel']
     options = get_leave_options( channel_id )
-    blocks= [
-		{   "block_id":"ApplyLeave",
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Apply Leave*"
-			}
-		},
-		{   "block_id":"type",
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Leave Type(Availaible Quotas)"
-			},
-			"accessory": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Sick",
-					"emoji": True
-				},
-				"options": options
-			}
-		},
-		{   "block_id":"fdate",
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Select start date"
-			},
-			"accessory": {
-				"type": "datepicker",
-				"initial_date": "1990-04-28",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select a date",
-					"emoji": True
-				}
-			}
-		},
-		{   "block_id":"tdate",
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Select end date."
-			},
-			"accessory": {
-				"type": "datepicker",
-				"initial_date": "1990-04-28",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select a date",
-					"emoji": True
-				}
-			}
-		},
-		{   "block_id":"submit",
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": True,
-						"text": "Cancel"
-					},
-					"style": "danger",
-					"value": "cancel"
-				},
-				{   
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"emoji": True,
-						"text": "Submit"
-					},
-					"style": "primary",
-					"value": "submit",
-                    "confirm": {
-						"title": {
-							"type": "plain_text",
-							"text": "Are you sure?"
-						},
-						"text": {
-							"type": "mrkdwn",
-							"text": "Apply for leave ?"
-						},
-						"confirm": {
-							"type": "plain_text",
-							"text": "Do it"
-						},
-						"deny": {
-							"type": "plain_text",
-							"text": "Stop, I've changed my mind!"
-						}
-					}
-				}
-			]
-		}
-	]
+
+    file_path = os.path.join( os.getenv('ROOT_PATH'),'python_files/slack/blocks/apply_leave/leave_type.json')
+    blocks = None
+    
+    with open(file_path,'r') as block_json:
+        blocks = json.load(block_json)
+    blocks[1]['accessory']['options'] = options    
     access_token =None #slack_helper.find_access_token(query['team_id'])
     slack_helper.send_message_to_slack(channel_id,blocks=blocks,access_token=access_token)
-   
-     
+    
 def get_leave_options( emp_code ):
     
 	url = "https://71f345c7-e619-4430-8261-a751682c1e51.mock.pstmn.io/api/leave/balance/read"
